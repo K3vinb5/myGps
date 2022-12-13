8ac2d0aa5b3fec0380fdbedcbcc66207049deb3a
K3vinb5 <kevinborgborges555@gmail.com>
2022-12-13 19:40:47 +0000

    updated installer

--- install/install.lua
+++ install/install.lua
@@ -1,8 +1,9 @@
 print("Creating myGps folder")
 fs.makeDir("myGps")
 shell.run("cd myGps")
-print("Downloading myGps.lua")
+--main
+shell.run("mkdir main")
+shell.run("cd main")
 shell.run("wget https://raw.githubusercontent.com/K3vinb5/myGps/main/main/myGps.lua")
-print("Downloading setup.lua")
 shell.run("wget https://raw.githubusercontent.com/K3vinb5/myGps/main/main/setup.lua")
 shell.run("cd ..")
