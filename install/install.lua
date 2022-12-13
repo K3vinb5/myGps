0e0f04e9ea872f50f65cbeb05db8523d011c8d61
K3vinb5 <kevinborgborges555@gmail.com>
2022-12-13 19:55:15 +0000

    Update install.lua

--- install/install.lua
+++ install/install.lua
@@ -4,6 +4,7 @@ shell.run("cd myGps")
 --main
 shell.run("mkdir main")
 shell.run("cd main")
-shell.run("wget https://raw.githubusercontent.com/K3vinb5/myGps/main/main/myGps.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/myGps/main/main/client.lua")
+shell.run("wget https://raw.githubusercontent.com/K3vinb5/myGps/main/main/server.lua")
 shell.run("wget https://raw.githubusercontent.com/K3vinb5/myGps/main/main/setup.lua")
 shell.run("cd ..")
